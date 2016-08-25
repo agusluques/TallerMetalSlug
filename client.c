23ee30c8c3d08e5e9d8e80ebc2140eba16b2d0ed
Agustin <Agus@MacBook.local>
2016-08-25 16:36:28 -0300

    Un poco mejorado

--- client.c
+++ client.c
@@ -15,38 +15,51 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
+    //VARIABLES
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
     char buffer[256];
     if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       fprintf(stderr,"Modo de uso: '%s hostname/IP portnumber'\n", argv[0]);
        exit(0);
     }
-    portno = atoi(argv[2]);
+
+    //SOCKET
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
-        error("ERROR opening socket");
+        error("ERROR, no se pudo abrir el socket");
+    
+    //VINCULA IP O HOSTNAME
     server = gethostbyname(argv[1]);
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
+        fprintf(stderr,"ERROR, no existe este hostname/IP\n");
         exit(0);
     }
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, 
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
+
+    portno = atoi(argv[2]);
     serv_addr.sin_port = htons(portno);
+
+    //CONNECT
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
+        error("ERROR, no se pudo conectar");
+    else printf("Conectado al servidor\n");
+
+    printf("Escriba su mensaje: ");
+
     bzero(buffer,256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
          error("ERROR writing to socket");
+    else printf("Mensaje enviado\n");
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
     if (n < 0) 
--- server.c
+++ server.c
@@ -16,40 +16,69 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+    //VARIABLES
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, escriba el puerto\n");
+        exit(1);
+    }
+
+    //SOCKET
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR, no se pudo abrir el servidor");
+    else printf("Servidor Abierto\n");   
+
+    //Inicializo serv_addr en 0
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+
+    serv_addr.sin_family = AF_INET;
+
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    portno = atoi(argv[1]);
+    serv_addr.sin_port = htons(portno);
+
+    //BIND
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+            sizeof(serv_addr)) < 0) 
+            error("ERROR on binding");
+
+    //LISTEN
+    listen(sockfd,5);
+    printf("Escuchando cliente...\n");
+
+    //ACCEPT
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, 
+                (struct sockaddr *) &cli_addr, 
+                &clilen);
+    if (newsockfd < 0) 
+        error("ERROR on accept");
+    else {
+        printf("Conectado!\n");
+        printf("Leyendo mensaje...\n");
+    }
+
+    //READ
+    bzero(buffer,256);
+    n = read(newsockfd,buffer,255);
+    if (n < 0) error("ERROR, no se pudo leer el mensaje");
+    
+    printf("Mensaje: %s\n",buffer);
+    
+    //WRITE
+    char msg[256]="Mensaje recibido por el servidor\n";
+    n = write(newsockfd,msg,strlen(msg));
+    if (n < 0) error("ERROR writing to socket");
+    
+    close(newsockfd);
+    close(sockfd);
+    return 0; 
 }
